8c6d993ee5311fc1a3e9a045516e1a7157ac9dee
Bonnie Brown <bonnie.brown@jane.app>
2022-12-01 02:23:59 +0000

    Added columns to trips tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_01_013750) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_021709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_01_013750) do
     t.string "destination"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "departure_date"
+    t.date "return_date"
+    t.text "image_url"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,17 @@ Trip.destroy_all
 
 10.times do
 
-    created_at = Faker::Date.backward(days: 365 * 5)
+    created_at = Faker::Date.backward(days: 365 * 1)
+    departure_date = Faker::Date.between(from: 5.days.ago, to: Date.today)
+    return_date = Faker::Date.between(from: '2023-01-30', to: '2024-01-30')
+    image_url = 'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
 
     Trip.create(
         name: Faker::Lorem.sentence(word_count: 3),
         destination: Faker::Lorem.word,
+        departure_date: departure_date,
+        return_date: return_date,
+        image_url: image_url,
         created_at: created_at,
         updated_at: created_at
     )
