cb71df9f8ede507ab37b407410e204491647ed63
Bonnie Brown <bonnie.brown@jane.app>
2022-12-11 01:49:16 +0000

    Continued to work on accommodations page

--- app/controllers/accommodations_controller.rb
+++ app/controllers/accommodations_controller.rb
@@ -1,7 +1,7 @@
 class AccommodationsController < ApplicationController
 
     before_action :find_trip
-    before_action :find_accommodation, only: [:show]
+    before_action :find_accommodation, only: [:show, :update, :destroy]
     
     before_action :authenticate_user!
 
@@ -37,13 +37,31 @@ class AccommodationsController < ApplicationController
 
     # @trip = @accommodation.trip
    
-    
-
    end
+
+   def edit
+   end
+
+   def update
+  
+        if @accommodation.update(accommodation_params)
+            flash[:success] = "Successfully updated!"
+            redirect_to @accommodation
+        else
+            flash[:error] = "Something went wrong"
+            render 'edit'
+        end
+   end
+
 
    #Destroy
 
    def destroy
+
+    @accommodation.destroy
+    flash[:success] = "Successfully deleted."
+    redirect_to trip_accommodations_path
+
    end
 
        private
